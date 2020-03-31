3d368ac04a94a4c18c447e7bdd6c669bddae2742
hoanglammaster <hoanglammaster@gmail.com>
2020-03-31 16:02:09 +0700

    UPDATE

---
+++ PE_SLOT6/Q1.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+int main() {
+  system("cls");
+  //INPUT - @STUDENT:ADD YOUR CODE FOR INPUT HERE:
+	int a;
+	scanf("%d",&a);
+  
+  
+  
+  
+  
+  // Fixed Do not edit anything here.
+  printf("\nOUTPUT:\n");
+  //@STUDENT: WRITE YOUR OUTPUT HERE:
+	printf("%0.6f",exp(a));
+
+
+
+
+
+
+
+  
+  
+  //--FIXED PART - DO NOT EDIT ANY THINGS HERE
+  printf("\n");
+  system ("pause");
+  return(0);
+}
